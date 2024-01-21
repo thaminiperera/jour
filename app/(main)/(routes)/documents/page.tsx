febfc6697d43c0065dceaad4cf5e74190e9269b4
Thamini Perera <thaminiperera@gmail.com>
2024-01-21 22:41:26 +0530

    user

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,10 +1,36 @@
-import { Navigation } from "../../_components/navigation";
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>
-      This is a protected Documents Page. And there is much to be said about
-      where this goes
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dak.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        {" "}
+        Welcome to {user?.firstName}&apos;s Jour
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" /> Create a Journal
+      </Button>
     </div>
   );
 };
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -112,7 +113,7 @@ export const Navigation = () => {
           <ChevronLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
